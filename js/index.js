6317bd07d59a97f86daa0bb1bdf183b0bca9cf0d
josemaria1988 <idontknow1988@gmail.com>
2023-05-09 23:39:53 -0300

    changing styles

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
     closeMenu.style.display = 'block';
     if (window.innerWidth <= 1024) {
-        hamburger.style.display = 'none'; // Oculta el botón de hamburguesa cuando se abre el menú en pantallas pequeñas
+        hamburger.style.display = 'none';
     }
 });
 
@@ -18,3 +18,34 @@ closeMenu.addEventListener('click', () => {
     }
 });
 
+// TWITTER
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+
+    return t;
+  }(document, "script", "twitter-wjs"));
+
+  twttr.ready(function (twttr) {
+    twttr.widgets.createTimeline(
+      {
+        sourceType: "profile",
+        screenName: "HolbertonUru"
+      },
+      document.getElementById("twitter-container"),
+      { height: 600 }
+    ).then(function () {
+      var loader = document.querySelector('.loader');
+      loader.style.display = 'none';
+    });
+  });
